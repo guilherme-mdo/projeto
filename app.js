f9132b62510b7906014cebc384b1e2603e8b00c4
Guilherme Michael <guilhermemd011@gmail.com>
2025-03-29 15:07:02 -0300

    ajustes hover+disposição

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    //responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
